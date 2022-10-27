a3894e7766ed373efbbc1c9971d2efc4e2a6a978
rajeshgogu <114799868+rajeshgogu@users.noreply.github.com>
2022-10-27 18:18:04 +0530

    Add files via upload

---
+++ probles strings.py
@@ -0,0 +1,9 @@
+#take a string of length 5[index 0-4]
+#print first char
+#print middle char
+#print last char
+a = "round"
+print(a[0])
+b = len(a)//2
+print(a[b])
+print(a[4])
---
+++ strings.py
@@ -0,0 +1,12 @@
+from email.quoprimime import quote
+
+
+quote = "i am rocky"
+if "rocky" not in quote:
+    print("yes")
+else:
+    print("no")    
+
+string = "RAJESH"
+print(string[5])
+print(len(string))
