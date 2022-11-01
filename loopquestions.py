7847f87ba1ba587c80b10772a3abcedcd351a53c
rajesh1234 <114799868+rajeshgogu@users.noreply.github.com>
2022-11-01 23:10:14 +0530

    Add files via upload

---
+++ loopquestions.py
@@ -0,0 +1,11 @@
+#accept a number from the user
+#calculate and print the sum of all the numbers
+#from 1 to input number
+#using for loop
+
+
+a = 4
+sum = 0
+for i in range(1,a+1):
+    sum = sum+i
+    print(i)
---
+++ newquestion.py
@@ -0,0 +1,7 @@
+#print first 10 natural numbers in reverse
+# using while loop 
+
+i = 10
+while i>=1:
+    print(i)
+    i = i-1
