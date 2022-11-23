f0deb3a7b9e116d337102531e9fad08d4d6dd441
rajesh1234 <114799868+rajeshgogu@users.noreply.github.com>
2022-11-23 17:41:57 +0530

    Delete calculator.py

--- calculator.py
+++
@@ -1,8 +0,0 @@
-def add(num1,num2):
-    print(num1+num2)
-x = lambda num1,num2: num1+num2
-y = lambda num1,num2: num1-num2
-z = lambda num1,num2: num1**num2
-a = lambda num1,num2: num1//num2
-b = lambda num1,num2: num1%num2
-print(b(5,6)) 
