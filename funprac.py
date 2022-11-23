005a3cd0bc39a151c43644bd66c4615176850dbd
rajesh1234 <114799868+rajeshgogu@users.noreply.github.com>
2022-11-23 17:44:07 +0530

    Delete funprac.py

--- funprac.py
+++
@@ -1,9 +0,0 @@
-def  name(myname):
-    print("my name is : "+myname)
-name("xyz")    
-
-def name(firstname ="rajesh"):
-    print("my name is:"+firstname)
-name()    
-name("vamsi")
-name("gowtham")    
